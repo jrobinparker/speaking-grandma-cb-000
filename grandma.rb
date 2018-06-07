ca446a9020c50d9d88c306b821e59b7bc5ef59ff
Jeremy Parker <jrobinparker@gmail.com>
2018-06-07 21:47:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
   
   if string != string.upcase && string != "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
-    elsif string = string.upcase
+    elsif string.upcase
       return "NO, NOT SINCE 1938!"
     elsif string = "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"

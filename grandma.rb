c231f7a88d52950496be440406956941d39cec8f
Jeremy Parker <jrobinparker@gmail.com>
2018-06-07 21:35:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   
   if string.downcase || string.capitalize
-    return "NO, NOT SINCE 1938!"
+    return HUH?! SPEAK UP, SONNY!
   elsif string.capitalize
     return 
   elsif 
